d4af514082f8f73d2b12c3a794ceb4d87620d94d
user664635 <myy12358@gmail.com>
2025-07-10 13:40:46 +0800

    tmp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 
 typedef uint32_t u32;
 #define regmap(addr) (*(volatile u32 *)(addr))
-#define GPIOA regmap(0x400a120c)
+#define GPIOA_D15_12 regmap(0x400a120c)
+#define GPIOASET regmap(0x400a1290)
+#define GPIOACLR regmap(0x400a12a0)
 
 extern "C" void reset() {
-  GPIOA = 0x01010101;
-  while (1)
-    ;
+  GPIOACLR = 0xffffffff;
+  GPIOASET = 0xffffffff;
+  while (1) {
+    GPIOA_D15_12 = 0x01010101;
+    GPIOA_D15_12 = 0;
+  }
 }
+
 extern "C" void stack_top();
 [[gnu::section(".text.ivt"), gnu::used]]
 void (*const ivt[256])(void) = {stack_top, reset};
